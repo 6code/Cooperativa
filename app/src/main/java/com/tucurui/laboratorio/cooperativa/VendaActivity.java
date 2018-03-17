902740211dcb7c894b31941bcb02887fc1085370
edu <eduadolima384@gmail.com>
2018-03-16 21:08:33 -0300

    venda activity

---
+++ app/src/main/java/com/tucurui/laboratorio/cooperativa/VendaActivity.java
@@ -0,0 +1,15 @@
+package com.tucurui.laboratorio.cooperativa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VendaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venda);
+
+
+    }
+}
--- app/src/main/java/com/tucurui/laboratorio/cooperativa/fragment/FragmentEstoque.java
+++ app/src/main/java/com/tucurui/laboratorio/cooperativa/fragment/FragmentEstoque.java
@@ -2,19 +2,26 @@ package com.tucurui.laboratorio.cooperativa.fragment;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.tucurui.laboratorio.cooperativa.R;
+import com.tucurui.laboratorio.cooperativa.VendaActivity;
 
 public class FragmentEstoque extends Fragment {
 
+    private AlertDialog alerta;
 
     //variavel para capturar contexto
     private Context context;
+
 
     //captura o contexto de quem me chamou
     @Override
@@ -28,8 +35,18 @@ public class FragmentEstoque extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_estoque, container, false);
 
+
+        Button venda = v.findViewById(R.id.id_venda);
+        venda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(context, VendaActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return v;
     }
-
 
 }
